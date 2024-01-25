14417f206a6a23d456df3d21708bef56bdcbe052
pedro <douglas.333.goncalves@gmail.com>
2024-01-25 16:00:08 -0300

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const numberAleatorio = (Math.random() * (100 - 1) + 1).toFixed(0);
 console.log(numberAleatorio)
 let tentatives=0
-let numberDigitado
+let numberEntered
 
-while (numberAleatorio != numberDigitado) {
- numberDigitado =  Number(prompt("Digite um numero de 1 a 100"))
-if(numberDigitado!=numberAleatorio){
-    if(numberDigitado > numberAleatorio){
-        alert(`O numero é menor que ${numberDigitado}`)
+while (numberAleatorio != numberEntered) {
+ numberEntered =  Number(prompt("Digite um numero de 1 a 100"))
+if(numberEntered!=numberAleatorio){
+    if(numberEntered > numberAleatorio){
+        alert(`O numero é menor que ${numberEntered}`)
     }
     else{
-        alert(`O numero é maior que ${numberDigitado}`)
+        alert(`O numero é maior que ${numberEntered}`)
     }
 }
 tentatives ++
 }
 let msgTentaives = tentatives >1?"tentativas":"tentativa"
 console.log(msgTentaives)
-if(numberAleatorio == numberDigitado){
+if(numberAleatorio == numberEntered){
 alert(`Parabéns você acertou o numero em ${tentatives} ${msgTentaives} `)
 
 }
